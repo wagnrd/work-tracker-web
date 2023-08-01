43f1459604daf9a6c1d9309b78983a6788fb7713
wagnrd <projects@wagnrd.de>
2023-08-01 22:12:23 +0200

    add dynamic platform options

--- src/pages/download/controller.rs
+++ src/pages/download/controller.rs
@@ -5,10 +5,67 @@ use crate::utils::ssr;
 
 #[derive(TemplateOnce)]
 #[template(path = "pages/download/page.html")]
-struct DownloadTemplate {}
+struct DownloadTemplate {
+    platform_options: String,
+}
+
+struct DownloadOption {
+    name: &'static str,
+    link_url: &'static str,
+}
+
+#[derive(TemplateOnce)]
+#[template(path = "pages/download/platform_options.html")]
+struct PlatformOptionsTemplate {
+    stores: Vec<DownloadOption>,
+    installers: Vec<DownloadOption>,
+    other_platforms: Vec<&'static str>,
+}
 
 #[get("/")]
 pub async fn page(request: actix_web::HttpRequest) -> impl actix_web::Responder {
-    let template = DownloadTemplate {};
+    let stores = vec![
+        DownloadOption {
+            name: "Flathub",
+            link_url: "https://flathub.org",
+        },
+        DownloadOption {
+            name: "Snap Store",
+            link_url: "https://snapcraft.io/store",
+        },
+    ];
+    let installers = vec![
+        DownloadOption {
+            name: "Flatpak",
+            link_url: ".",
+        },
+        DownloadOption {
+            name: "Snap",
+            link_url: ".",
+        },
+        DownloadOption {
+            name: "AppImage",
+            link_url: ".",
+        },
+        DownloadOption {
+            name: "DEB",
+            link_url: ".",
+        },
+        DownloadOption {
+            name: "RPM",
+            link_url: ".",
+        },
+    ];
+    let other_platforms = vec!["macOS", "Windows"];
+
+    let platform_options = PlatformOptionsTemplate {
+        stores,
+        installers,
+        other_platforms,
+    }
+    .render_once()
+    .unwrap();
+
+    let template = DownloadTemplate { platform_options };
     ssr::render_page(template, request)
 }
