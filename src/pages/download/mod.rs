a521d8e672f9230363dc67047684b66fbc65c8c7
Denis Wagner <denis.wagner@check24.de>
2023-08-03 09:55:52 +0200

    changes project to svelte kit

--- src/pages/download/mod.rs
+++
@@ -1,7 +0,0 @@
-use actix_web;
-
-mod controller;
-
-pub fn scope() -> actix_web::Scope {
-    actix_web::web::scope("/download").service(controller::page)
-}
--- src/utils/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod auto_refresh;
-pub mod ssr;
